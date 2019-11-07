ef8f2d8d8e9cf8d345d7236580cce348ac64e5c7
xiehaokun <1830136212@qq.com>
2019-11-07 15:37:20 +0800

    注释

---
+++ src/views/home.vue
@@ -0,0 +1,58 @@
+<template>
+   <div id="app">
+       <div class="shang">
+         <router-link to="">为您推荐</router-link>
+         <router-link to="">高薪职位</router-link>
+         <router-link to="">福利专区</router-link>
+       </div>
+       <div class="xia">
+         <router-view></router-view>
+       </div>
+     
+   </div>
+</template>
+
+<script>
+export default {
+   comments:{
+
+   },
+   methods: {
+     
+   },
+   mounted() {
+     
+   },    
+}
+
+</script>
+
+
+<style lang="scss">
+#app{
+    width:100%;
+    height:100%;
+    box-sizing: border-box;
+  
+}
+
+.shang{
+    width:100%;
+    height:50px;
+    border:1px solid black;
+    display:flex;
+    justify-content: space-around;
+    align-items: center;
+}
+
+.xia{
+    width:100%;
+    height:450px;
+    border:1px solid black;
+}
+
+
+
+
+
+</style>
